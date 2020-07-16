dedbf178a33816d0e90066924504d5fbc4b9e151
hasha2982 <hashadev@yandex.ru>
2020-07-16 11:04:41 +0300

    1.0.1

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open('README.md', 'r') as file:
     readme = file.read()
 
 setuptools.setup(
-    name="wttrpy", # Replace with your own username
-    version="1.0.0",
+    name="wttrpy",
+    version="1.0.1",
     author="Denisov Artem",
     author_email="hashadev@yandex.ru",
     description="A little thing on Python that shows you forecast using wttr.in",
@@ -18,6 +18,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    install_requires=['requests>=2,<3']
+    python_requires='>=3.5',
+    install_requires=['requests>=2,<3'],
+    project_urls={
+        'Documentation': "https://wttrpy.rtfd.io/",
+        'Source Code': "https://github.com/hasha2982/wttr.py/",
+        'Issues': "https://github.com/hasha2982/wttr.py/issues/"
+    }
 )
